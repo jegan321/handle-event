ec29993843a3cab2c090ecb21c3ed5e18fddb7be
John Egan <eganj@dnb.com>
2020-02-08 09:55:34 -0600

    Added comment

--- handle-event.js
+++ handle-event.js
@@ -1,3 +1,20 @@
+/**
+ * Adds an event handler to be called using the event delegation technique. The HTML
+ * element that should trigger the handler needs to have an attribute following this
+ * pattern: handle-{type}="{name}"
+ * 
+ * For example, an event handler can be added like this:
+ * handleEvent('click', 'doSomething', function (event) {
+ *   console.log('clicked element: ' + event.target)
+ * })
+ * 
+ * And the HTML will have the corresponding attribute like this:
+ * <button type="button" handle-click="doSomething">Click Me</button>
+ * 
+ * @param {String} type The type of event to handle, such as click or submit
+ * @param {String} name A name for the handler
+ * @param {Function} callback The function that will be called when the event happens
+ */
 var handleEvent = function (type, name, callback) {
   if (!handleEvent.handlers[type]) {
     handleEvent.handlers[type] = {}
